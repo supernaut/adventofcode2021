b3b601c29d8f46a5e2a07ddeea47259764dc0523
Johannes Axner <johannes.axner@gmail.com>
2021-12-02 20:28:24 +0100

    refactor(day-1): optimized increase calculation for speed

--- day1/calculateIncreases.js
+++ day1/calculateIncreases.js
@@ -1,6 +1,4 @@
 module.exports = (values) =>
   values
-    .filter((row) => !!row)
-    .map((row) => parseInt(row, 10))
-    .map((value, index, all) => value > all[index - 1] || 0)
+    .map((value, index, all) => (value > all[index - 1] ? 1 : 0))
     .reduce((a, b) => a + b, 0);

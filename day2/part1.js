4aacbc0c19857bd9a21378df6fdc1370f44b7b2f
Johannes Axner <johannes.axner@gmail.com>
2021-12-02 20:46:47 +0100

    feat(day-2): add solution for day 2 part 1

---
+++ day2/part1.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+const data = fs.readFileSync(path.resolve(__dirname, "input.txt"), "utf8");
+
+const position = {
+  horizontal: 0,
+  depth: 0,
+};
+
+data
+  .split("\n")
+  .filter((row) => !!row)
+  .forEach((row) => {
+    const [direction, raw] = row.trim().split(" ");
+    const value = parseInt(raw, 10);
+    switch (direction) {
+      case "forward":
+        position.horizontal += value;
+        break;
+      case "down":
+        position.depth += value;
+        break;
+      case "up":
+        position.depth -= value;
+        break;
+    }
+  });
+
+console.log(position.horizontal * position.depth);

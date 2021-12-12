b852d3a9103e501c8fcc01461a5a93f512b123c2
Johannes Axner <johannes@axner.io>
2021-12-12 15:33:38 +0100

    refactor(day-5): optimize part 2 from 300s to 18ms

--- day5/day5-part2.ts
+++ day5/day5-part2.ts
@@ -1,45 +1,30 @@
 import { solution, title } from "../shared/output";
-import { input as data, VentLine } from "./data";
 
-title(5, 1);
+import { input as data } from "./data";
+import { hrtime } from "process";
 
-const points = data.flatMap(({ from, to }: VentLine) => {
-  const line: string[] = [];
-  const diffX = Math.abs(from.x - to.x);
-  const diffY = Math.abs(from.y - to.y);
-  const steps = Math.max(diffX, diffY) - 1;
-  const intervalX = diffX / (steps + 1);
-  const intervalY = diffY / (steps + 1);
+title(5, 2);
+const start = hrtime();
 
-  line.push(`${from.x},${from.y}`);
+const size = 1024;
+const arrayBuffer = new ArrayBuffer(size * size);
+const buffer = new Int8Array(arrayBuffer).fill(0);
 
-  for (let i = 1; i <= steps; i++) {
+const getIndex = (x: number, y: number): number => x + y * size;
+
+for (let l = 0; l < data.length; l += 1) {
+  const { from, to } = data[l];
+  const steps = Math.max(Math.abs(from.x - to.x), Math.abs(from.y - to.y));
+
+  for (let i = 0; i <= steps; i += 1) {
     const x =
-      from.x === to.x
-        ? from.x
-        : from.x < to.x
-        ? from.x + intervalX * i
-        : from.x - intervalX * i;
+      from.x === to.x ? from.x : from.x < to.x ? from.x + i : from.x - i;
     const y =
-      from.y === to.y
-        ? from.y
-        : from.y < to.y
-        ? from.y + intervalY * i
-        : from.y - intervalY * i;
-    line.push(`${x},${y}`);
+      from.y === to.y ? from.y : from.y < to.y ? from.y + i : from.y - i;
+    buffer[getIndex(x, y)]++;
   }
-  line.push(`${to.x},${to.y}`);
+}
 
-  return line;
-});
+const count = buffer.filter((value) => value > 1).length;
 
-const count = points
-  .map((point) => points.filter((value) => value === point))
-  .filter((value) => value.length > 1)
-  .map((value) => value[0])
-  .filter(
-    (value: string, index: number, array: string[]) =>
-      array.indexOf(value) === index
-  ).length;
-
-solution(count);
+solution(count, hrtime(start));

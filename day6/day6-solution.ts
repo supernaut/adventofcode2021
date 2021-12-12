325e4a0d4faf6e1ccc63bc9134e900ce7fe6e368
Johannes Axner <johannes@axner.io>
2021-12-12 18:59:28 +0100

    fix(day-6): add attribution

--- day6/day6-solution.ts
+++ day6/day6-solution.ts
@@ -1,3 +1,5 @@
+// Hat tip to https://www.reddit.com/user/blakjakau/
+
 export const solve = (input: number[], days: number): number => {
   // One slot per days left
   const states = new Array(9).fill(0);
@@ -11,13 +13,13 @@ export const solve = (input: number[], days: number): number => {
   // Loop over days
   for (let i = 0, l = days; i < l; i += 1) {
     // First state is birthing, shift out those fish
-    const b = states.shift();
+    const fish = states.shift();
 
     // Add a new fish for each birthing one
-    states.push(b);
+    states.push(fish);
 
     // Add the fish that birthed to day 6
-    states[6] += b;
+    states[6] += fish;
   }
 
   // Sum up the fish of all states

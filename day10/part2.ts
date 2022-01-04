a2b0cff857637f3dcefa39a278302872702261e8
Johannes Axner <johannes.axner@gmail.com>
2022-01-04 01:10:16 +0100

    feat(day-10): add solution for part 2

---
+++ day10/part2.ts
@@ -0,0 +1,51 @@
+import { hrtime } from "process";
+import { summary, title } from "../shared/output";
+import { input as data } from "./data";
+
+const start = hrtime();
+title(10, 2);
+
+const cleanPairs = /(\(\)|<>|\[\]|\{\})/g;
+
+const solve = (input: Array<string>): number => {
+  const scores: Array<number> = input
+    .map((row) => {
+      let clean = row;
+      while (clean.match(cleanPairs)?.length) {
+        clean = clean.replace(cleanPairs, "");
+      }
+      return clean;
+    })
+    .filter((value) => !value.match(/(>|\}|\]|\))/g)?.length)
+    .map((value) => {
+      let score = 0;
+      [...value].reverse().forEach((char) => {
+        switch (char) {
+          case "(":
+            score *= 5;
+            score += 1;
+            return ")";
+          case "[":
+            score *= 5;
+            score += 2;
+            return "]";
+          case "{":
+            score *= 5;
+            score += 3;
+            return "}";
+          case "<":
+            score *= 5;
+            score += 4;
+            return ">";
+        }
+      });
+      return score;
+    })
+    .sort((a, b) => b - a);
+
+  return scores[Math.floor(scores.length / 2)];
+};
+
+const solution = solve(data);
+
+summary(solution, hrtime(start));
